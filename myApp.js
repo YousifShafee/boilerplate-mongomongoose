f89d70a4279c971ef942f98ca87af4ad91824745
Yousif Shafee <yousiftariq1@hotmail.com>
2022-02-06 18:23:54 +0200

    Perform New Updates on a Document

--- myApp.js
+++ myApp.js
@@ -63,13 +63,9 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-  Person.findOne({ name: personName }, (err, data) => {
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
     if (err) return console.error(err);
-    data.age = ageToSet;
-    data.save((err, data) => {
-      if (err) return console.error(err);
-      done(null, data);
-    });
+    done(null, data);
   });
 };
 
